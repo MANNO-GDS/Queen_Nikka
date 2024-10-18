488e950d3e889e864c30d10c6a9ba4c09826c24b
MANNO-GDS <153306254+MANNO-GDS@users.noreply.github.com>
2024-10-18 18:19:46 +0500

    💌 𝙏𝙃𝙀-𝙈𝘼𝙉𝙉𝙊-𝙈𝘿 💌

--- plugins/repo.js
+++ plugins/repo.js
@@ -4,25 +4,24 @@ const { smd } = require("../lib");
 
 smd({
   pattern: "botrepo",
-  react: "📁",
+  react: "💌",
   alias: ["repo", "bot"],
   desc: "Sends info about the WhatsApp bot repository",
   category: "general",
   filename: __filename
 }, async (message) => {
   try {
-    const response = await axios.get("https://api.github.com/repos/hakisolos/Queen_Nikka");
+    const response = await axios.get("https://github.com/MANNO-GDS/Queen_Nikka");
     const repoData = response.data;
 
-    let replyMessage = `**🍀WhatsApp Bot Repository Info**\n\n`;
-    replyMessage += `**🍀Repository Name:** ${repoData.name}\n`;
-    replyMessage += `**🍀Description:** ${repoData.description || 'No description available'}\n`;
-    replyMessage += `**🍀Stars:** ${repoData.stargazers_count}\n`;
-    replyMessage += `**🍀Forks:** ${repoData.forks_count}\n`;
-    replyMessage += `**🍀Watchers:** ${repoData.watchers_count}\n`;
-    replyMessage += `**🍀Open Issues:** ${repoData.open_issues_count}\n`;
-    replyMessage += `**🍀License:** ${repoData.license ? repoData.license.name : 'No License'}\n`;
-    replyMessage += `**🍀Repository URL:** [Click here]👉(https://github.com/hakisolos/Queen_Nikka)\n`;
+    let replyMessage = `*📃WhatsApp Bot Repository Info*\n\n`;
+    replyMessage += `*🩹Repository Name:* ${repoData.name}\n`;
+    replyMessage += `*💭Description:* ${repoData.description || 'No description available'}\n`;
+    replyMessage += `*⭐Stars:* ${repoData.stargazers_count}\n`;
+    replyMessage += `📊Forks:* ${repoData.forks_count}\n`;
+    replyMessage += `🌚Watchers:* ${repoData.watchers_count}\n`;
+    replyMessage += `💭Open Issues:* ${repoData.open_issues_count}\n`;
+    replyMessage += `🧭Repository URL:* [Click here]👉(https://github.com/MANNO-GDS/Queen_Nikka)\n`;
 
     await message.send(replyMessage);
   } catch (error) {
